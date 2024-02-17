08189d7660529fc09a7bdd936cd58358a91d12f2
Вика <ruseckaya@cmit.ru>
2024-02-17 18:53:58 +0300

    start project

---
+++ example1/.section.php
@@ -0,0 +1,6 @@
+<?php
+$sSectionName="example1";
+$arDirProperties = [
+    "keywords" => "Example, Two",
+    "description" => "My page Example 2",
+];
---
+++ local/templates/scooter_album/description.php
@@ -0,0 +1,8 @@
+<?php
+
+$arTemplate = [
+    "NAME" => "Шаблон главной страницы Scooter",
+    "DESCRIPTION" => "Это шаблон лендинга",
+    "SORT" => 100,
+    "TYPE" => "",
+];
